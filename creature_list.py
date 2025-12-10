7d19d8808b7c23db4c27a3fd4871baf73b616713
whose-stone <whosestone@gmail.com>
2025-12-09 17:03:04 -0700

    create a monster

--- creature_list.py
+++ creature_list.py
@@ -1,74 +1,68 @@
 # create a monster. All monsters have a name, type, life, and body.
 
-water_body = '\033[34m           \n   ^  ^   \n    @     \n  /   \\ \n    JL    \n\033[0m'
-earth_body = '\033[32m          \n   *  *   \n   -$-    \n  <~~>    \n   000    \n\033[0m'
-fire_body = '\033[31m          \n   )  (   \n   00    \n  ###     \n  {] [}    \n\033[0m'
+WATER_BODY = ('\033[34m           \n   ^  ^   \n    @     \n  /   \\ \n    JL    \n\033[0m')
+EARTH_BODY = ('\033[32m          \n   *  *   \n   -$-    \n  <~~>    \n   000    \n\033[0m')
+FIRE_BODY = ('\033[31m          \n   )  (   \n   00    \n  ###     \n  {] [}    \n\033[0m')
 
 
 
 
 class Creature:
-    def __init__(self, name, life, attacks, type):
-        self.creature_name = name
-        self.creature_life = life
-        self.creature_attacks = attacks
-        self.creature_type = type
+    def __init__(self, name, life, attacks, creature_type):
+        self.name = name
+        self.life = life
+        self.attacks = attacks
+        self.type = creature_type
 
     def set_name(self, name):
-        self.creature_name = name
+        self.name = name
     
     def set_life(self, life):
-        self.creature_life = life
+        self.life = life
 
-    def set_attack(self, attacks):
-        self.creature_attacks = attacks
+    def set_attacks(self, attacks):
+        self.attacks = attacks
 
     def get_name(self):
-        return self.creature_name
+        return self.name
     
     def get_life(self):
-        return self.creature_life
+        return self.life
     
     def get_attacks(self):
-        return self.creature_attacks
+        return self.attacks
     
-    def take_hit(self, hit_value):
+    def take_hit(self, hit_value, inbound_attack="Attack", opponent_name="Opponent"):
         if self.life > hit_value:
-            self.life = self.life - hit_value
-            print(f'{inbound_attack} of {hit_value} hurts {self.get_name}!')
+            self.life -= hit_value
+            print(f'{inbound_attack} of {hit_value} hurts {self.get_name()}!')
             return self.life
         else:
             self.life = 0
-            print(f'{opponent_name} destroyed {self.creature_name}. You lose!')
+            print(f'{opponent_name} destroyed {self.name}. You lose!')
+
 
     
 class WaterCreature(Creature):
+    def __init__(self, name, life=100):
+        attacks = {'Wash': 5, 'Spew': 8, 'Dunk': 7}
+        super().__init__(name, life, attacks, "Water")
+        self.body = WATER_BODY
 
-    def __init__(self, name, life = 100, attacks = {'Wash': 5, 'Spew': 8, 'Dunk':7}, type = "Water"):
-        super().__init__(name, life, attacks, type)
-        self.name = name
-        self.type = type
-        self.life = life
-        self.attacks = attacks
-        self.body = '\033[34m           \n   ^  ^   \n    @     \n  /   \\ \n    JL    \n\033[0m'
 
 class EarthCreature(Creature):
+    def __init__(self, name, life=130):
+        attacks = {'Crumble': 3, 'Smash': 6, 'Dig': 5}
+        super().__init__(name, life, attacks, "Earth")
+        self.body = EARTH_BODY
 
-    def __init__(self, name, life, creature_type):
-        super().__init__( name, life)
-        self.life = 130
-        self.creature_type = creature_type
-        self.attacks = {'Crumble': 3, 'Smash': 6, 'Dig':5}
-        self.body = '\033[32m          \n   *  *   \n   -$-    \n  <~~>    \n   000    \n\033[0m'
 
 class FireCreature(Creature):
+    def __init__(self, name, life=80):
+        attacks = {'Blaze': 8, 'Torch': 10, 'Simmer': 5}
+        super().__init__(name, life, attacks, "Fire")
+        self.body = FIRE_BODY
 
-    def __init__(self, name, life, creature_type):
-        super().__init__( name, life)
-        self.life = 80
-        self.creature_type = creature_type
-        self.attacks = {'Blaze': 8, 'Torch': 10, 'Simmer':5}
-        self.body = '\033[31m          \n   )  (   \n   00    \n  ###     \n  {] [}    \n\033[0m'
 
 def render_character(character):
     print(character.body)
@@ -82,8 +76,5 @@ def render_character(character):
 
 
 
-#Ainew = WaterCreature('Aniew')
-#
-#render_character(Ainew)
-
-print(WaterCreature.body)
+Ainew = WaterCreature('Aniew')
+render_character(Ainew)
--- main.py
+++ main.py
@@ -4,7 +4,31 @@ from creature_list import *
 
 input("Welcome to MONSTER MASH! Where you will create and fight your own monster! Press Enter to continue...")
 
-print("You can choose from three kinds of monsters. Water, Earth, and Fire"\n )
+print("You can choose from three kinds of monsters. Water, Earth, and Fire \n"
+      f"Water {WATER_BODY}\n"
+      f"Earth {EARTH_BODY}\n"
+      f"Fire {FIRE_BODY}\n"
+      )
+
+def create_monster():
+    name = input("Enter your monster's name: ")
+    monster_type = input("Choose a type (Water, Earth, Fire): ").lower()
+
+    if monster_type == "water":
+        monster = WaterCreature(name)
+    elif monster_type == "earth":
+        monster = EarthCreature(name)
+    elif monster_type == "fire":
+        monster = FireCreature(name)
+    else:
+        print("Invalid type! Defaulting to Water.")
+        monster = WaterCreature(name)
+
+    return monster
+
+user_monster = create_monster()
+print(f"\nMonster created!\nName: {my_monster.name}\nType: {my_monster.type}\nLife: {my_monster.life}")
+print(my_monster.body)
 
 
 
